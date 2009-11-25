2c1dd071df2f9a44064742de05291a05575944f4
Yesudeep Mangalapilly <yesudeep@happychickoo.com>
2009-11-25 15:49:31 +0530

    render_template can now be generated

--- app/utilities.py
+++ app/utilities.py
@@ -24,10 +24,10 @@
 # THE SOFTWARE.
 
 import configuration
-from jinja2 import Environment, FileSystemLoader
 from functools import partial
 from haggoo.template.jinja2.filters import datetimeformat
 from haggoo.template.jinja2.loaders import PythonLoader
+from jinja2 import Environment, FileSystemLoader
 from urllib import urlencode
 
 # Set up the Jinja2 Environment to use the correct loader.
@@ -39,22 +39,26 @@ else:
     template_loader = FileSystemLoader
 
 jinja_env = Environment(loader=template_loader(template_directories))
-jinja_env.filters(['datetimeformat']) = datetimeformat
+jinja_env.filters['datetimeformat'] = datetimeformat
 jinja_env.filters['urlencode'] = urlencode
 
-def render_template(template_name, **context, template_builtins=configuration.TEMPLATE_BUILTINS):
-    """
-    Fills a template with values provided as context.
+def render_template_generator(template_builtins=configuration.TEMPLATE_BUILTINS):
+    def render_template(template_name, **context):
+        """
+        Fills a template with values provided as context.
     
-    The function also includes a few template builtins that are available to all 
-    rendered templates.
+        The function also includes a few template builtins that are available to all 
+        rendered templates.
     
-    """
-    template = jinja_env.get_template(template_name)
-    new_context = {}
-    new_context.update(template_builtins)
-    new_context.update(context)
-    return template.render(new_context)
+        """
+        template = jinja_env.get_template(template_name)
+        new_context = {}
+        new_context.update(template_builtins)
+        new_context.update(context)
+        return template.render(new_context)
+    return render_template
+
+render_template = render_template_generator()
 
 # Conveninence wrapper to make sure int conversion uses a decimal base. 
 dec = partial(int, base=10)
